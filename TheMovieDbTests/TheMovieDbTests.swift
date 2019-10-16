a0293f2e354e4b8177a632805905ca2c2b2b43c8
Márcio Brufatto <mhbrufatto@gmail.com>
2019-10-16 17:37:06 -0300

    Initial Commit

---
+++ TheMovieDbTests/TheMovieDbTests.swift
@@ -0,0 +1,34 @@
+//
+//  TheMovieDbTests.swift
+//  TheMovieDbTests
+//
+//  Created by Marcio Habigzang Brufatto on 16/10/19.
+//  Copyright © 2019 Marcio Habigzang Brufatto. All rights reserved.
+//
+
+import XCTest
+@testable import TheMovieDb
+
+class TheMovieDbTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
