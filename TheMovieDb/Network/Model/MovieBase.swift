f1dec74776174b2365dea214350eafe09e9ce892
Márcio Brufatto <mhbrufatto@gmail.com>
2019-10-16 19:08:10 -0300

    - Criada a tela inicial carregando os dados do webservice.

---
+++ TheMovieDb/Network/Model/MovieBase.swift
@@ -0,0 +1,22 @@
+//
+//  MovieBase.swift
+//  TheMovieDb
+//
+//  Created by Marcio Habigzang Brufatto on 16/10/19.
+//  Copyright © 2019 Marcio Habigzang Brufatto. All rights reserved.
+//
+
+import Foundation
+
+class MovieBase: Codable {
+    
+    var totalResults: Int
+    var totalPages: Int
+    var movies: [Movie] = []
+    
+    enum CodingKeys: String, CodingKey {
+        case totalResults = "total_results"
+        case totalPages = "total_pages"
+        case movies = "results"
+    }
+}
